67823733b892e2c6e1d4f2e89b12da89904b194e
TanyaT_homepc <tanya.yt.tan@gmail.com>
2020-06-16 09:25:12 -0700

    switch hard-coded input to user input

--- disaster_response_msg/data/process_data.py
+++ disaster_response_msg/data/process_data.py
@@ -11,12 +11,6 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 
-# provide/modify filenames 
-messages_filepath ="messages.csv"
-categories_filepath="categories.csv"
-database_filepath="disaster_response.db"
-database_tablename="disaster_messages"
-
 
 def load_data(messages_filepath, categories_filepath):
     '''
@@ -76,21 +70,32 @@ def save_data(df,database_filepath):
     '''
     # load to database
     engine = create_engine("sqlite:///"+database_filepath)
-    df.to_sql(database_tablename, engine, index=False)
+    df.to_sql("disaster_messages", engine, index=False,if_exists="replace")
    
 def main():
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
+        if len(sys.argv)==4:
+            messages_filepath,categories_filepath,database_filepath = sys.argv[1]
+       
+            print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'.format(messages_filepath, categories_filepath))
+            df = load_data(messages_filepath, categories_filepath)
 
-        print('Cleaning data...')
-        df = clean_data(df)
+            print('Cleaning data...')
+            df = clean_data(df)
         
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
+            print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+            save_data(df, database_filepath)
         
-        print('Cleaned data saved to database!')
+            print('Cleaned data saved to database!')
+        
+        else:
+            print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+            
     
 
 if __name__ == '__main__':
