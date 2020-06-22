2fa6e364d7c0bfaa33586ce12b8abd1ae73b00f9
TanyaT_homepc <tanya.yt.tan@gmail.com>
2020-06-22 13:23:22 -0700

    Update process_data.py

--- disaster_response_msg/data/process_data.py
+++ disaster_response_msg/data/process_data.py
@@ -25,7 +25,7 @@ def load_data(messages_filepath, categories_filepath):
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     
-    # merge data on id
+    # inner join data on id
     df=pd.merge(messages,categories,on="id")
     
     return df
